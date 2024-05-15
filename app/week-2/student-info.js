82b38e9d8523ed38f8a9d1ae778720e06eb00c6d
ZoeeeLin <152560065+ZoeeeLin@users.noreply.github.com>
2024-05-14 23:59:26 -0600

    Finish assignment 2 tasks
    Add a folder called week-2, one page and one component for assignment 2.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    // const linkStyles = "underline text-cyan-600";
+    return (
+      <main className="h-screen">
+        <h1>Yi-Chen Lin</h1>
+        <Link className="linkStyles" href="https://github.com/ZoeeeLin/cprg306-assignments">https://github.com/ZoeeeLin/cprg306-assignments</Link>
+      </main>
+    );
+  }
